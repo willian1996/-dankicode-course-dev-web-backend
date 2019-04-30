92ba2e81d1412913fdc7804c576934b29dc6686b
Willian Sales <williansalesgabriel@hotmail.com>
2019-04-30 02:12:15 -0300

    Aula 30/04

--- Aula48-Projeto-Site-Dinamico-3/index.php
+++ Aula48-Projeto-Site-Dinamico-3/index.php
@@ -42,85 +42,21 @@
         </div><!--center-->
         <div class="clear"></div><!--clear-->
     </header>
+    <?php
     
-    <section class="banner-principal">
-    <div class="overlay"></div><!--overlay-->
-        <div class="center">
-            <form>
-                <h2>Qual o seu melhor e-mail?</h2>
-                <input type="email" name="email" required/>
-                <input type="submit" name="acao" value="Cadastrar!"/>
-            </form>
-        </div><!--center-->
-    </section><!--banner-principal-->
+    $url = isset($_GET['url'])?$_GET['url'] : 'home';
     
-    <section class="descricao-autor">
-        <div class="center">
-            <div class="w50 left">
-                <h2>Willian Sales Gabriel</h2>
-                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
-                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum.</p>
-            </div><!--w50-->
-            <div class="w50 left">
-                <img class="right" src="img/williansales.jpg">
-            </div><!--w50-->
-            <div class="clear"></div>
-        </div><!--center-->
-    </section><!--descricao-autor-->
     
-    <section class="especialidades">
-        <div class="center">
-            <h2 class="title">Especialidades</h2>
-            <div class="w33 left box-especialidade">
-                <h3><i class="fab fa-css3-alt"></i></h3>
-                <h4>CSS3</h4>
-                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
-            </div><!--especialidade-->
-            <div class="w33 left box-especialidade">
-                <h3><i class="fab fa-html5"></i></h3>
-                <h4>HTML5</h4>
-                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
-            </div><!--especialidade-->
-            <div class="w33 left box-especialidade">
-                <h3><i class="fab fa-js-square"></i></h3>
-                <h4>JavaScript</h4>
-                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
-            </div><!--boxespecialidade-->
-            <div class="clear"></div>
-        </div><!--center-->
-    </section><!--especialidades-->
+    if(file_exists('pages/'.$url.'.php')){
+        include('pages/'.$url.'.php');
+    }else{
+        include('pages/404.php');
+    }
     
-    <section class="extras">
-        <div class="center">
-            <div class="w50 left depoimentos-container">
-                <h2 class="title">Depoimentos dos nossos clientes</h2>
-                <div class="depoimento-single">
-                    <p class="depoimento-descricao">"Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos..."</p>
-                    <p class="nome-autor">Fulano da Silva</p>
-                </div><!--depoimentos-single-->
-                <div class="depoimento-single">
-                    <p class="depoimento-descricao">"Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos..."</p>
-                    <p class="nome-autor">Ciclano de Oliveira</p>
-                </div><!--depoimentos-single-->
-                <div class="depoimento-single">
-                    <p class="depoimento-descricao">"Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos..."</p>
-                    <p class="nome-autor">Beltrano dos Santos</p>
-                </div><!--depoimentos-single-->
-            </div><!--w50-->
-            <div class="w50 left servicos-container">
-                <h2 class="title">Serviços</h2>
-                <div class="servicos">
-                    <ul>
-                        <li>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</li>
-                        <li>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</li>
-                        <li>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</li>
-                    </ul>
-                </div><!--servicos-->
-            </div><!--w50-->
-            <div class="clear"></div>
-        </div><!--center-->
-    </section><!--extras-->
     
+    
+    
+    ?>
     <footer>
         <div class="center">
             <p>Todos os direitos reservas</p>
---
+++ Aula48-Projeto-Site-Dinamico-3/pages/404.php
@@ -0,0 +1,12 @@
+
+<section class="404">
+    <div class="center">
+        <h2><i class="fa fa-times"></i>A página não existe!</h2>
+    
+    
+    </div>
+
+
+
+
+</section>
